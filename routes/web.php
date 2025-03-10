48032cf2f5e83fe578090b4182dd18e595056566
Alijalal-Dev <aj0546006@gmail.com>
2025-03-10 16:19:38 +0100

    upload dashboard and modify the Sidebar

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('LandingPage.index');
 });
+
+
+
+Route::get('/dashboard', function () {
+    return view('Dashboard.index');
+});
+
